143eb9d030f2a879b37ed4ba363f98daadcff19a
Eunseo Park <75926505+parkes811-inu@users.noreply.github.com>
2024-01-23 23:55:40 +0900

    Title: 더하기 사이클, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240123/더하기 사이클/additional-cycles.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int answer;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    
+    cin >> n;
+    int sum = 0;
+    int a = n / 10;
+    int b = n % 10;
+
+    while(1) {
+
+        if(n == sum) {
+            break;
+        }
+
+        sum = a + b;
+        sum = ((a + b) % 10) + (b * 10);
+        //cout << sum << '\n';
+        answer++;
+
+        a = sum / 10;
+        b = sum % 10;
+    }
+    cout << answer;
+    return 0;
+}
