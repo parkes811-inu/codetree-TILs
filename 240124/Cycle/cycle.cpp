ca42819c838caff572c5ad5327f5e82ef3bb247c
Eunseo Park <75926505+parkes811-inu@users.noreply.github.com>
2024-01-24 00:46:25 +0900

    Title: Cycle, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240124/Cycle/cycle.cpp
+++ 240124/Cycle/cycle.cpp
@@ -5,33 +5,22 @@
 using namespace std;
 
 int main() {
-    // 여기에 코드를 작성해주세요.
     int n, p;
     cin >> n >> p;
 
-    vector<int> answer;
-    int temp = (n * n) % p;
-    answer.push_back(temp);
-
-    while(1) {
-        temp = (temp * n) % p;
-        if(find(answer.begin(), answer.end(), temp) != answer.end()) {
-            cout << answer.size() << ' ';
+    vector<int> sequence;
+    unordered_map<int, int> position;
+    int current = n;
+    while (true) {
+        current = (current * n) % p;
+        // 이미 등장했던 숫자가 등장하면 사이클의 크기를 출력
+        if (position.find(current) != position.end()) {
+            cout << sequence.size() - position[current] << endl;
             break;
         }
-        answer.push_back(temp);    
-    }    
-
-    // int temp = n * n;
-    // temp = temp % p;
-    // cout << temp << '\n';
-
-    // temp = temp * n;
-    // temp = temp % p;
-    // cout << temp << '\n';
-
-    // temp = temp * n;
-    // temp = temp % p;
-    // cout << temp << '\n';
+        // 숫자와 그 위치를 저장
+        position[current] = sequence.size();
+        sequence.push_back(current);
+    }
     return 0;
 }
