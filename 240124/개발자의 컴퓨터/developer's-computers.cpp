ecb8571c7dee50a50515f9dae85bb4b9e9632170
Eunseo Park <75926505+parkes811-inu@users.noreply.github.com>
2024-01-24 16:59:22 +0900

    Title: 개발자의 컴퓨터, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240124/개발자의 컴퓨터/developer's-computers.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    int s, t, b;
+
+    cin >> n;
+    
+    vector<int> time (1000, 0);
+    for(int i = 0; i < n; i++) {
+        cin >> s >> t >> b;
+
+        for(int j = s; j < t; j++) {
+            time[j] = time[j] + b;
+        }
+    }
+
+    sort(time.begin(), time.end());
+
+    cout << time[time.size() - 1];
+    return 0;
+}
