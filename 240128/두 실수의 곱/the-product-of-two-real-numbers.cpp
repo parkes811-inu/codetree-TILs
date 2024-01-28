1b638ba957f1580591dcd479051c8103ce3f6086
Eunseo Park <75926505+parkes811-inu@users.noreply.github.com>
2024-01-28 14:19:06 +0900

    Title: 두 실수의 곱, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240128/두 실수의 곱/the-product-of-two-real-numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+float a = 5.26, b = 8.27;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cout << fixed;
+    cout.precision(3);
+    cout << a * b;
+    return 0;
+}
