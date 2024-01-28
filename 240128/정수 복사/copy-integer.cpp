f8fd8348a36675a2dcda2abb0ef57e6c87447543
Eunseo Park <75926505+parkes811-inu@users.noreply.github.com>
2024-01-28 14:21:23 +0900

    Title: 정수 복사, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240128/정수 복사/copy-integer.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int a, b;
+
+int main() {
+    a = 3; b = 4;
+    b = a;
+    // 여기에 코드를 작성해주세요.
+    cout << a << ' ' << b << '\n';
+    cout << a * b;
+    return 0;
+}
