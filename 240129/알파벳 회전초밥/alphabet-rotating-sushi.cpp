bf8574b9d281bb904997490d70504688712bb0db
Eunseo Park <75926505+parkes811-inu@users.noreply.github.com>
2024-01-29 23:45:15 +0900

    Title: 알파벳 회전초밥, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240129/알파벳 회전초밥/alphabet-rotating-sushi.cpp
+++ 240129/알파벳 회전초밥/alphabet-rotating-sushi.cpp
@@ -1,41 +1,26 @@
 #include <iostream>
 #include <string>
-#include <vector>
 
 using namespace std;
 
-string a;
-string b;
-vector<pair<char, int>> answer;
-string result = "";
-int count = 1;
-
 int main() {
-    // 여기에 코드를 작성해주세요.
-    cin >> a >> b;
-    // for(int i = 0; i < a.size(); i++) {
-    //     answer.push_back(make_pair(a[i], i));
-    // }
-    int i = 0; int j = 0;
+    string lst;
+    string word;
+    cin >> lst;
+    cin >> word;
 
-    while(1) {
-        if(j == b.size()) {
-            cout << count;
-            break;
-        }
+    int bf = 0;
+    int res = 1;
 
-        // if(answer[i].first == b[j]) {
-        //     j++;
-        // }
-        if(a[i] == b[j]) {
-                j++;     
+    for (int i = 0; i < word.size(); i++) {
+        size_t num = lst.find(word[i]);
+        if (bf >= num) {
+            res += 1;
         }
+        bf = num;
+    }
 
-        i++;
-        if(i == 25) {
-            count++;
-            i = 0;
-        }
-    }   
+    cout << res << endl;
+
     return 0;
 }
