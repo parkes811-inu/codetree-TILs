9543d220fb25425eb83e1f3656439b479e53a8d7
Eunseo Park <75926505+parkes811-inu@users.noreply.github.com>
2024-01-30 00:12:10 +0900

    Title: 알파벳 회전초밥, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240130/알파벳 회전초밥/alphabet-rotating-sushi.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string a;
+string b;
+int count = 0;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> a >> b;
+    int idx = 0;
+    for(int i = 0; i < b.size(); i++) {
+
+        for(int j = 0; j < 26; j++) {
+            if(b[idx] == a[j]) {
+                idx++;
+            }
+        }
+        count++;
+    }
+    cout << count;
+    //cout << b.size();  
+    return 0;
+}
