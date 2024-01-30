add7200fc68e9120eab88645f2f17d325928c933
Eunseo Park <75926505+parkes811-inu@users.noreply.github.com>
2024-01-30 17:14:54 +0900

    Title: 수의 곱셈, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240130/수의 곱셈/multiplication-of-numbers.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int num[3];
+bool cmp[3];
+int answer;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    // for(int i = 0; i < 3; i++) {
+    //     cin >> num[i];
+
+    //     if(num[i] % 2 != 0) {
+    //         cmp[i] = true;
+    //     }
+    // }
+
+    // for(int i = 0; i < 3; i++) {
+
+    //     if(cmp[i] == true) {
+
+    //     }
+    // }
+    int a, b, c;
+
+    cin >> a >> b >> c;
+
+    cout << a * b * c;
+
+    return 0;
+}
