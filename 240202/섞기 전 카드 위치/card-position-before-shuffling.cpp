f0c037a039376c12b8e735ce56577bb34d795a9e
Eunseo Park <75926505+parkes811-inu@users.noreply.github.com>
2024-02-02 15:36:13 +0900

    Title: 섞기 전 카드 위치, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240202/섞기 전 카드 위치/card-position-before-shuffling.cpp
+++ 240202/섞기 전 카드 위치/card-position-before-shuffling.cpp
@@ -1,33 +1,38 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
-int n;
-
-bool compare(pair<int, int> a, pair<int, int> b) {
-    if(a.second < b.second) {
-        return true;
-    }
-    return false;
-}
-
 int main() {
-    // 여기에 코드를 작성해주세요.
+    int n;
     cin >> n;
 
-    vector<pair<int, int>> list(n);
+    vector<int> sequence(n), final_state(n), initial_state(n);
+    vector<int> position(n);
 
-    for(int i = 0; i < n; i++) {
-        cin >> list[i].first;
+    // 수열 A를 입력받아 위치 매핑을 생성합니다.
+    for (int i = 0; i < n; i++) {
+        cin >> sequence[i];
+        position[sequence[i] - 1] = i;
     }
-    for(int i = 0; i < n; i++) {
-        cin >> list[i].second;
+
+    // 최종 카드 상태를 입력받습니다.
+    for (int i = 0; i < n; i++) {
+        cin >> final_state[i];
     }
-    sort(list.begin(), list.end());
-    for(int i = 0; i < n; i++) {
-        cout << list[i].second << '\n';
+
+    // 카드를 섞는 과정을 3번 역으로 수행합니다.
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < n; j++) {
+            initial_state[position[j]] = final_state[j];
+        }
+        final_state = initial_state;
     }
+
+    // 초기 상태를 출력합니다.
+    for (int i = 0; i < n; i++) {
+        cout << initial_state[i] << '\n';
+    }
+
     return 0;
 }
