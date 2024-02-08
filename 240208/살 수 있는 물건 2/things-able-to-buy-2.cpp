5614992587d3a3f548b6dec5dfa9f8c45cc8f066
Eunseo Park <75926505+parkes811-inu@users.noreply.github.com>
2024-02-08 15:36:44 +0900

    Title: 살 수 있는 물건 2, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240208/살 수 있는 물건 2/things-able-to-buy-2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+
+    if(n >= 3000) {
+        cout << "book";
+    }
+    else if(n >= 1000 && n <3000) {
+        cout << "mask";
+    }
+    else if(n >= 500 && n < 1000) {
+        cout << "pen";
+    }
+    else {
+        cout << "no";
+    }
+    return 0;
+}
