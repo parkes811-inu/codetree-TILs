40adb464e2876c1cc8a16c33f1c7deea11ae9558
Eunseo Park <75926505+parkes811-inu@users.noreply.github.com>
2024-02-08 17:18:28 +0900

    Title: 조건에 따른 연산, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240208/조건에 따른 연산/operatino-by-rule.cpp
+++ 240208/조건에 따른 연산/operatino-by-rule.cpp
@@ -12,11 +12,11 @@ int main() {
             break;
         }
 
-        if(n % 2 == 0) {
+        else if(n % 2 == 0) {
             n = (n * 3) + 1;
             cnt++;
         }
-        if(n % 2 != 0) {
+        else if(n % 2 != 0) {
             n = (n * 2) + 2;
             cnt++;
         }
