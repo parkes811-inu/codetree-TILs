ae455b0dc87adef059e0d903e1a848de9f4a0dcb
Eunseo Park <75926505+parkes811-inu@users.noreply.github.com>
2024-02-08 15:27:17 +0900

    Title: 합과 평균의 차, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240208/합과 평균의 차/sub-of-average-and-sum.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << a+b+c << '\n' << (a+b+c)/3 << '\n' << (a+b+c) - ((a+b+c)/3);
+    return 0;
+}
