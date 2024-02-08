d65644bb12c8d9a1c8678ded3e0892fceec4f26f
Eunseo Park <75926505+parkes811-inu@users.noreply.github.com>
2024-02-08 15:33:11 +0900

    Title: 삼항연산자 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240208/삼항연산자 2/ternary-operator-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    cin >> a;
+    string result = (a == 1) ? "t" : "f";
+    cout << result;
+    return 0;
+}
