56683b1673954c383dc9aa968d42db14b8ae4b16
Eunseo Park <75926505+parkes811-inu@users.noreply.github.com>
2024-02-08 17:14:56 +0900

    Title: 조건에 따른 연산, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240208/조건에 따른 연산/operatino-by-rule.cpp
+++ 240208/조건에 따른 연산/operatino-by-rule.cpp
@@ -8,11 +8,6 @@ int main() {
     cin >> n;
     while(1) {
 
-        if(n > 1000) {
-            cout << cnt;
-            break;
-        }
-
         if(n % 2 == 0) {
             n = (n * 3) + 1;
             cnt++;
@@ -21,6 +16,11 @@ int main() {
         if(n % 2 != 0) {
             n = (n * 2) + 2;
             cnt++;
+        }
+
+        if(n > 1000) {
+            cout << cnt;
+            break;
         }
     }
     return 0;
