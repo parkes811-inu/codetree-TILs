4d3a38b7b3982741de3dfa6bb029d50983040396
Eunseo Park <75926505+parkes811-inu@users.noreply.github.com>
2024-02-13 17:57:37 +0900

    Title: 나무박멸, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240213/나무박멸/tree-kill-all.cpp
+++ 240213/나무박멸/tree-kill-all.cpp
@@ -45,7 +45,7 @@ void GrowTree() {
 }
 
 // 2-1. 각 칸의 나무 그 수에서 총 번식이 가능한 칸의 개수만큼 나누어진 만큼 번식
-int breedingCnt(int x, int y) {
+void breedingCnt(int x, int y) {
     int cnt = 0;
 
     for(int i = 0; i < 4; i++) {
@@ -56,36 +56,40 @@ int breedingCnt(int x, int y) {
             continue;
         }
 
-        if(map[nx][ny] == 0 && map[x][y] > 0) {
+        if(map[nx][ny] == 0) {
             cnt++;
         }
     }
 
-    return cnt;
+    
+
+    if(cnt > 0) {
+        int plusCnt = map[x][y] / cnt;
+        // / 연산으로 나머지는 버림.
+        for(int k = 0; k < 4; k++) {
+        int nx = x + dx[k];
+        int ny = y + dy[k];
+
+        if(nx < 0 || ny < 0 || nx > n || ny > n || map[x][y] <= 0 || 
+            map[nx][ny] != 0) {
+            continue;
+        }
+
+        map[nx][ny] = plusCnt;
+    }
+
+    }
+    
+    return;
 }   
 
 // 2. 기존에 있던 나무들은 인접한 4개의 칸 중 벽, 다른 나무, 제초제가 모두 없는 칸에 번식
 void breeding() {
     
-    // / 연산으로 나머지는 버림.
-
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
-
-            int cnt = breedingCnt(i, j);
-
-            if(cnt > 0) {
-                for(int k = 0; k < 4; k++) {
-                    int nx = i + dx[k];
-                    int ny = j + dy[k];
-
-                    if(nx < 0 || ny < 0 || nx > n || ny > n || map[i][j] == -1) {
-                        continue;
-                    }
-                    if(map[nx][ny] == 0) {
-                        map[nx][ny] = 999;
-                    }
-                }
+            if(map[i][j] != 0 ) {
+                breedingCnt(i, j);
             }
         }
     }
