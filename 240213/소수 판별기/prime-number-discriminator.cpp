f908b40323526e65d60d78086b6281bce8a7d163
Eunseo Park <75926505+parkes811-inu@users.noreply.github.com>
2024-02-13 13:08:35 +0900

    Title: 소수 판별기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240213/소수 판별기/prime-number-discriminator.cpp
+++ 240213/소수 판별기/prime-number-discriminator.cpp
@@ -5,15 +5,19 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     int n;
-
+    int cnt = 0;
     cin >> n;
 
     for(int i = 1; i <= n; i++) {
-        if(n % i == 0 && i == 1 || i == n) {
-            cout << "P";
-            return 0;
+        if(n % i == 0) {
+            cnt++;
         }
     }
-    cout << "C";
+    if(cnt == 2) {
+        cout << "P";
+    }
+    else {
+        cout << "C";
+    }
     return 0;
 }
