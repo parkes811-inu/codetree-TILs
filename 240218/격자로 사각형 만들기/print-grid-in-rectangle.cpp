b756adb110a9c74165ea11dac07dba8993543308
Eunseo Park <75926505+parkes811-inu@users.noreply.github.com>
2024-02-18 01:01:18 +0900

    Title: 격자로 사각형 만들기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240218/격자로 사각형 만들기/print-grid-in-rectangle.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int arr[10][10];
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        arr[i][0] = 1;
+        arr[0][i] = 1;
+    }
+    for(int i = 1; i < n; i++) {
+        for(int j = 1; j < n; j++) {
+            arr[i][j] = arr[i - 1][j] + arr[i][j - 1] + arr[i - 1][j - 1];
+        }
+    }
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << arr[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+    return 0;
+}
