65bdf4a06c85f029b23ddbeea0907b0ef6c5efa7
Eunseo Park <75926505+parkes811-inu@users.noreply.github.com>
2024-02-19 16:16:23 +0900

    Title: 문자에 따른 명령 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240219/문자에 따른 명령 2/text-based-commands2.cpp
+++ 240219/문자에 따른 명령 2/text-based-commands2.cpp
@@ -3,9 +3,11 @@ using namespace std;
 
 string cmd;
 
-//int dx[4] = {0, 1, 0, -1};
-//int dy[4] = {-1, 0, -1, 0};
-int dx[4] = {1, 0, -1, 0}, dy[4] = {0, -1, 0, 1};
+int dx[4] = {0, 1, 0, -1};
+int dy[4] = {1, 0, -1, 0};
+
+//int dx[4] = {1, 0, -1, 0};
+//int dy[4] = {0, -1, 0, 1};
 
 int dir = 0;
 pair<int, int> cur;
@@ -14,8 +16,7 @@ int main() {
     // 여기에 코드를 작성해주세요.
     cin >> cmd;
     for(int i = 0; i < cmd.size(); i++) {
-        int nx = cur.first + nx;
-        int ny = cur.second + ny;
+
         if(cmd[i] == 'L') {
             dir = (dir - 1 + 4) % 4;
         }
