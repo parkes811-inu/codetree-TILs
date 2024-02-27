28dd7242c839fbf411abf192abd04500dcbf0e7b
Eunseo Park <75926505+parkes811-inu@users.noreply.github.com>
2024-02-27 16:31:07 +0900

    Title: 비오는 날, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240227/비오는 날/rainy-day.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <tuple>
+#include <vector>
+
+using namespace std;
+int n;
+vector<tuple<string, string, string>> weather;
+
+bool cmp(tuple<string, string, string> a, tuple<string, string, string> b) {
+    if(get<0>(a) < get<0>(b)) {
+        return true;
+    }
+    return false;
+}
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+        string a, b, c;
+        cin >> a >> b >> c;
+        weather.push_back(make_tuple(a, b, c));
+    }
+
+    for(int i = 0; i < n; i++) {
+        if(get<2>(weather[i]) == "Rain") {
+            cout << get<0>(weather[i]) << ' ';
+            cout << get<1>(weather[i]) << ' ';
+            cout << get<2>(weather[i]) << '\n';
+            return 0;
+        }
+    }
+    return 0;
+}
