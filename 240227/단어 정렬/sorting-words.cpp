3e3bd8227a8920a70e2fcb2f841f2db9a9788f1c
Eunseo Park <75926505+parkes811-inu@users.noreply.github.com>
2024-02-27 16:55:15 +0900

    Title: 단어 정렬, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240227/단어 정렬/sorting-words.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+vector<string> answer;
+string s;
+int n;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+        cin >> s;
+        answer.push_back(s);
+    }
+
+    sort(answer.begin(), answer.end());
+
+    for(int i = 0; i < answer.size(); i++) {
+        cout << answer[i] << '\n';
+    }
+    return 0;
+}
