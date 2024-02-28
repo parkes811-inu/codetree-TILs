27fea3c87b9f793836627ad047c2a72e27e6a7c9
Eunseo Park <75926505+parkes811-inu@users.noreply.github.com>
2024-02-28 12:41:46 +0900

    Title: 줄 세우기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240228/줄 세우기 2/line-up-students-2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <tuple>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+struct student {
+    int height;
+    int weight;
+    int num;
+};
+vector<student> list;
+
+bool cmp(student a, student b) {
+    if(a.height == b.height) {
+        return a.weight > b.weight;
+    }
+    return a.weight < b.weight;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+
+    for(int i = 1; i <= n; i++) {
+        int height, weight;
+        cin >> height >> weight;
+        list.push_back({height, weight, i});
+    }
+    sort(list.begin(), list.end(), cmp);
+
+    for(int i = 0; i < n; i++) {
+        cout << list[i].height << ' ' <<  list[i].weight <<
+        ' ' << list[i].num << '\n';
+    }
+    return 0;
+}
