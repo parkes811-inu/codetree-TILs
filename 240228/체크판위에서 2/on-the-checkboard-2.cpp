bce72e1f428a67aefaf41c99b818124a22b3c104
Eunseo Park <75926505+parkes811-inu@users.noreply.github.com>
2024-02-28 17:23:56 +0900

    Title: 체크판위에서 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240228/체크판위에서 2/on-the-checkboard-2.cpp
+++ 240228/체크판위에서 2/on-the-checkboard-2.cpp
@@ -14,12 +14,12 @@ int main() {
         }
     }
 
-    for(int i = 1; i < r; i++) {
-        for(int j = 1; j < c; j++) {
-            for(int k = i + 1; k < r - 1; k++) {
-                for(int l = j + 1; l < c -1 ; l++) {
-                    if(map[0][0] != map[i][j] && map[i][j] != map[k][l] && 
-                       map[k][l] != map[r-1][c-1]) {
+    for(int i = 1; i <= r; i++) {
+        for(int j = 1; j <= c; j++) {
+            for(int k = i + 1; k < r; k++) {
+                for(int l = j + 1; l < c ; l++) {
+                    if(map[1][1] != map[i][j] && map[i][j] != map[k][l] && 
+                       map[k][l] != map[r][c]) {
                         cnt++;
                     }
                 }
