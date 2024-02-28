ea05bdf1a4cdaf47539b2c74d217fb4535cba40c
Eunseo Park <75926505+parkes811-inu@users.noreply.github.com>
2024-02-28 13:11:15 +0900

    Title: 모이자, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240228/모이자/gather.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int arr[101];
+int n;
+int mini = 999;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    for(int i = 0; i < n; i++) {
+        int sum = 0;
+        for(int j = 0; j < n; j++) {
+            if(i == j) {
+                continue;
+            }
+            sum = sum + arr[j] * abs(i - j);
+        }
+        if(sum < mini) {
+            mini = sum;
+        }
+    }
+    cout << mini;
+    return 0;
+}
