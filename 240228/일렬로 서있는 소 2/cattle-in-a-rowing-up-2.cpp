34cd7788373803dcdffdc5c51512884b6d6f04a4
Eunseo Park <75926505+parkes811-inu@users.noreply.github.com>
2024-02-28 16:07:27 +0900

    Title: 일렬로 서있는 소 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240228/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.cpp
+++ 240228/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 using namespace std;
 int n;
-int arr[101];
+int arr[1000];
 int main() {
     // 여기에 코드를 작성해주세요.
     cin >> n;
-    for(int i = 0; i < n; i++) {
+    for(int i = 1; i <= n; i++) {
         cin >> arr[i];
     }
 
     int cnt = 0;
 
-    for(int i = 0; i < n; i++) {
-        for(int j = i + 1; j < n; j++) {
-            for(int k = j + 1; k < n; k++) {
-                if(arr[i] < arr[j] && arr[j] < arr[k] && arr[i] < arr[k]) {
+    for(int i = 1; i <= n; i++) {
+        for(int j = i + 1; j <= n; j++) {
+            for(int k = j + 1; k <= n; k++) {
+                if(arr[i] <= arr[j] && arr[j] <= arr[k]) {
                     cnt++;
                 }
             }
