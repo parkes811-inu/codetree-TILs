01574e2575fa225c3e31367b40a4b5ddab665a0f
Eunseo Park <75926505+parkes811-inu@users.noreply.github.com>
2024-03-04 16:36:53 +0900

    Title: 문자열을 출력하는 함수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240304/문자열을 출력하는 함수/function-that-outputs-string.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int n;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+        cout << "CodeTree!\n";
+    }
+    return 0;
+}
