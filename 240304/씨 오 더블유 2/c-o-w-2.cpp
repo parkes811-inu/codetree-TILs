9015c2b7e7af0b85fbf2505cb2404218e3794482
Eunseo Park <75926505+parkes811-inu@users.noreply.github.com>
2024-03-04 11:18:53 +0900

    Title: 씨 오 더블유 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240304/씨 오 더블유 2/c-o-w-2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int n;
+char a[101];
+int cnt;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+    for(int i = 0; i < n; i++) {    
+        cin >> a[i];
+    }
+
+    for(int i = 0; i < n - 2; i++) {
+        for(int j = i + 1; j < n - 1; j++) {
+            for(int k = i + 2; k < n; k++) {
+                if(a[i] == 'C' && a[j] == 'O' && a[k] == 'W') {
+                    cnt++;
+                }
+            }
+        }
+    }
+    cout << cnt;
+    return 0;
+}
