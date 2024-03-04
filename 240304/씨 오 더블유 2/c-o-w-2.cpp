25e04ae5fc407f29e1764e8978f3366bf49d651c
Eunseo Park <75926505+parkes811-inu@users.noreply.github.com>
2024-03-04 11:39:10 +0900

    Title: 씨 오 더블유 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240304/씨 오 더블유 2/c-o-w-2.cpp
+++ 240304/씨 오 더블유 2/c-o-w-2.cpp
@@ -11,11 +11,8 @@ int main() {
     }
 
     for(int i = 0; i < n - 2; i++) {
-        for(int j = 0; j < n - 1; j++) {
-            for(int k = 0; k < n; k++) {
-                if(i == j || i == k || j == k) {
-                    continue;
-                }
+        for(int j = i + 1; j < n - 1; j++) {
+            for(int k = j + 1; k < n; k++) {
                 if(a[i] == 'C' && a[j] == 'O' && a[k] == 'W') {
                     cnt++;
                 }
