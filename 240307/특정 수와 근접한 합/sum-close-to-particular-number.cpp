02d50b625a749ecd0b46d32bb72b454095d0d573
박은서 <75926505+parkes811-inu@users.noreply.github.com>
2024-03-07 15:03:03 +0900

    Title: 특정 수와 근접한 합, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240307/특정 수와 근접한 합/sum-close-to-particular-number.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+int n, s;
+int answer = 99999;
+int sum;
+int arr[102];
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n >> s;
+
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+        sum = sum + arr[i];
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            int temp = sum - (arr[i] + arr[j]);
+            answer = min(answer, abs(temp - s));
+        }
+    }
+    cout << answer;
+    return 0;
+}
