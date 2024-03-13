7156437ba316939d950c2c6298afaa17ea26854b
박은서 <75926505+parkes811-inu@users.noreply.github.com>
2024-03-14 00:44:55 +0900

    Title: 단 한 번의 2048 시도, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240314/단 한 번의 2048 시도/one-trial-of-2048-game.cpp
+++ 240314/단 한 번의 2048 시도/one-trial-of-2048-game.cpp
@@ -1,59 +1,76 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 const int N = 4;
 vector<vector<int>> board(N, vector<int>(N));
 
-void rotateBoard(char d) {
-    vector<vector<int>> temp(N, vector<int>(N));
-    if (d == 'R') { // 오른쪽으로 이동하는 경우 180도 회전
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                temp[i][j] = board[N - 1 - i][N - 1 - j];
-            }
-        }
-    } else if (d == 'U') { // 위로 이동하는 경우 270도 회전
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                temp[j][N - 1 - i] = board[i][j];
-            }
-        }
-    } else if (d == 'D') { // 아래로 이동하는 경우 90도 회전
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                temp[N - 1 - j][i] = board[i][j];
-            }
+void moveTiles(vector<int>& line) {
+    vector<int> newLine;
+    for (int i = 0; i < line.size(); i++) {
+        if (line[i] != 0) {
+            newLine.push_back(line[i]);
         }
     }
-    if (d != 'L') { // 회전된 보드를 다시 board에 복사
-        board = temp;
+
+    for (int i = 0; i < newLine.size() - 1; i++) {
+        if (newLine[i] == newLine[i + 1]) {
+            newLine[i] *= 2;
+            newLine[i + 1] = 0;
+        }
+    }
+
+    line.clear();
+    for (int i = 0; i < newLine.size(); i++) {
+        if (newLine[i] != 0) {
+            line.push_back(newLine[i]);
+        }
+    }
+
+    while (line.size() < N) {
+        line.push_back(0);
     }
 }
 
-void compressAndMerge() {
-    for (int i = 0; i < N; i++) {
-        vector<int> line;
-        for (int j = 0; j < N; j++) {
-            if (board[i][j] != 0) {
-                line.push_back(board[i][j]);
-            }
+void rotateBoard() {
+    vector<vector<int>> temp(N, vector<int>(N));
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            temp[j][N - 1 - i] = board[i][j];
         }
-        for (int j = 0; j < line.size() - 1; j++) {
-            if (line[j] == line[j + 1]) {
-                line[j] *= 2;
-                line[j + 1] = 0;
-            }
-        }
-        vector<int> newLine;
-        for (int j = 0; j < line.size(); j++) {
-            if (line[j] != 0) {
-                newLine.push_back(line[j]);
-            }
-        }
-        newLine.resize(N, 0);
-        board[i] = newLine;
     }
+    board = temp;
+}
+
+void moveLeft() {
+    for (int i = 0; i < N; i++) {
+        moveTiles(board[i]);
+    }
+}
+
+void moveRight() {
+    for (int i = 0; i < N; i++) {
+        reverse(board[i].begin(), board[i].end());
+        moveTiles(board[i]);
+        reverse(board[i].begin(), board[i].end());
+    }
+}
+
+void moveUp() {
+    rotateBoard(); // Rotate clockwise
+    moveLeft();
+    rotateBoard(); // 3 times to get original orientation
+    rotateBoard();
+    rotateBoard();
+}
+
+void moveDown() {
+    rotateBoard(); // Rotate clockwise 3 times
+    rotateBoard();
+    rotateBoard();
+    moveLeft();
+    rotateBoard(); // Once to get original orientation
 }
 
 void printBoard() {
@@ -72,17 +89,25 @@ int main() {
         }
     }
 
-    char d;
-    cin >> d; // 방향 입력 ('L', 'R', 'U', 'D')
+    char dir;
+    cin >> dir; // 방향 입력 ('L', 'R', 'U', 'D')
 
-    rotateBoard(d); // 입력된 방향에 따라 보드 회전
-    compressAndMerge(); // 숫자들을 압축하고 합침
-    if (d == 'R' || d == 'U' || d == 'D') { // 회전된 보드를 원래대로 복귀
-        rotateBoard('D'); rotateBoard('D');
-        if (d == 'U') rotateBoard('D');
-        if (d == 'D') rotateBoard('U');
+    switch (dir) {
+        case 'L':
+            moveLeft();
+            break;
+        case 'R':
+            moveRight();
+            break;
+        case 'U':
+            moveUp();
+            break;
+        case 'D':
+            moveDown();
+            break;
     }
-    printBoard(); // 결과 출력
+
+    printBoard(); // 최종 결과 출력
 
     return 0;
 }
