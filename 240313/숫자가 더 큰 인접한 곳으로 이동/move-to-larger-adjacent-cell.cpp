1566cf514fcc58dbe2503bfa141da80f4fa1a3c0
박은서 <75926505+parkes811-inu@users.noreply.github.com>
2024-03-13 12:07:21 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: 1095ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240313/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n;
+int r, c;
+int map[102][102];
+vector<int> answer;
+
+// 상, 하, 좌, 우 순서
+int dx[4] = {-1, 1, 0, 0};
+int dy[4] = {0, 0, -1, 1};
+
+int dir = 0;
+
+bool InRange(int x, int y) {
+    return (x > 0 && y > 0 && x <= n && y <= n);
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n >> r >> c;
+
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            cin >> map[i][j];
+        }
+    }
+
+    int x = r;
+    int y = c;
+    int nx = x;
+    int ny = y;
+    answer.push_back(map[x][y]);
+    int a = 5;
+
+    while(1) {
+        bool flag = true;
+
+        for(int i = 0; i < 4; i++) {
+            nx = x + dx[i];
+            ny = y + dy[i];
+
+            if(InRange(nx, ny) == false) {
+                continue;
+            }
+
+            if(map[nx][ny] > map[x][y]) {
+                x = nx;
+                y = ny;
+                answer.push_back(map[nx][ny]);
+                break;
+            }
+            nx = -1;
+            ny = -1;
+        }
+        
+        if(nx == -1 && ny == -1) {
+            break;
+        }
+    }
+
+    for(int i = 0; i < answer.size(); i++) {
+        cout << answer[i] << ' ';
+    }
+    return 0;
+}
