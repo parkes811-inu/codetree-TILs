28fc78d2c1cdb372b8e332c1f9e6ae31b5c43096
박은서 <75926505+parkes811-inu@users.noreply.github.com>
2024-03-19 16:31:39 +0900

    Title: 핀볼게임, Time: 1016ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 240319/핀볼게임/pinball-game.cpp
+++ 240319/핀볼게임/pinball-game.cpp
@@ -1,59 +1,56 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int n;
-int map[102][102];
-int dx[4] = {1, 0, -1, 0}; // 아래, 왼쪽, 위, 오른쪽
-int dy[4] = {0, -1, 0, 1};
-
-// 방향 변화 정의
-// reflectDir[type][currentDir] => 반사판 타입과 현재 방향에 따른 새로운 방향
+vector<vector<int>> board;
+int dx[4] = {0, 1, 0, -1}; // 오른쪽, 아래, 왼쪽, 위
+int dy[4] = {1, 0, -1, 0};
+// 반사판에서의 방향 변경
+// reflectDir[반사판 타입][현재 방향] = 새로운 방향
 int reflectDir[3][4] = {
-    {0, 0, 0, 0}, // 0은 반사판 없음, 방향 변화 없음
-    {1, 0, 3, 2}, // '/' 반사판에서의 방향 변화
-    {3, 2, 1, 0}  // '\' 반사판에서의 방향 변화
+    {0, 0, 0, 0}, // 0: 빈 공간, 방향 변경 없음
+    {3, 2, 1, 0}, // 1: / 모양 반사판
+    {1, 0, 3, 2}  // 2: \ 모양 반사판
 };
 
-bool InRange(int x, int y) {
-    return (x >= 0 && y >= 0 && x < n && y < n);
+bool inRange(int x, int y) {
+    return x >= 0 && x < n && y >= 0 && y < n;
 }
 
-int moveMarble(int x, int y, int dir) {
+int simulate(int x, int y, int dir) {
     int cnt = 0;
-    
-    while(true) {
+    while (true) {
         x += dx[dir];
         y += dy[dir];
-        cnt++; // 구슬을 움직임
-        
-        if (!InRange(x, y)) break; // 격자 밖으로 나가는 경우 게임 종료
-        
-        if (map[x][y] > 0) { // 반사판에 부딪힌 경우
-            dir = reflectDir[map[x][y]][dir]; // 방향 변경
+        cnt++;
+        if (!inRange(x, y)) break; // 격자 밖으로 나가면 종료
+        if (board[x][y] != 0) { // 반사판을 만나면 방향 변경
+            dir = reflectDir[board[x][y]][dir];
         }
     }
-
-    return cnt; // 격자 밖으로 나가기까지 걸린 시간 반환
+    return cnt;
 }
 
 int main() {
     cin >> n;
-    
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            cin >> map[i][j];
+    board.resize(n, vector<int>(n));
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cin >> board[i][j];
         }
     }
-    
-    int answer = 0;
-    // 격자의 모든 가능한 시작점에서 구슬을 이동
-    for(int i = 0; i < 4 * n; i++) {
-        // 여기에 시작점을 결정하는 로직을 추가
-        int cnt = moveMarble(시작 x, 시작 y, 시작 방향);
-        answer = max(answer, cnt);
+
+    int maxTime = 0;
+    // 모든 가능한 시작점에서 시뮬레이션
+    for (int i = 0; i < 4 * n; ++i) {
+        int x = i < n ? 0 : i < 2 * n ? i - n : n - 1; // x 좌표 결정
+        int y = i < n ? i : i < 2 * n ? n - 1 : 3 * n - i - 1; // y 좌표 결정
+        int dir = i < n ? 1 : i < 2 * n ? 2 : 3; // 방향 결정
+        maxTime = max(maxTime, simulate(x, y, dir));
     }
-    
-    cout << answer << endl;
-    
+
+    cout << maxTime + 1 << endl;
     return 0;
 }
