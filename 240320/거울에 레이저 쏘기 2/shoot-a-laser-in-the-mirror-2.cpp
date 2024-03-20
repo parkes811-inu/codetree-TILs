1d1b2a65258013d5e4bd34c5f9cf8c4624b41efa
박은서 <75926505+parkes811-inu@users.noreply.github.com>
2024-03-20 12:04:42 +0900

    Title: 거울에 레이저 쏘기 2, Time: 15ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240320/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.cpp
+++ 240320/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.cpp
@@ -9,6 +9,7 @@ int answer;
 // 시계 방향
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, -1, 0, 1};
+
 void print() {
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
@@ -20,35 +21,32 @@ void print() {
 
 int findDir(int k) {
     
-    for(int i = 1; i <= 4 * n; i++) {
-        if(i / 4 == 0) {
-            // cout << "i / 4 = 0 : " << i % 4 << ' ';
-            x = -1;
-            y = i % 4;
-            return 0;
-        }
-        else if(i / 4 == 1) {
-            // cout << "i / 4 = 1 : " << i % 4 << ' ';
-            x = i % 4;
-            y = n;
-            return 1;
-        }
-        else if(i / 4 == 2) {
-            // cout << "i / 4 = 2 : " << i % 4 << ' ';
-            x = n;
-            y = i % 4;
-            return 2;
-        }
-        else {
-            // cout << "i / 4 = 3 : " << i % 4 << ' ';
-            x = i % 4;
-            y = -1;
-            return 3;
-        }
-
-        // cout << '\n';
+    if(k < 4) {
+        // cout << "i / 4 = 0 : " << i % 4 << ' ';
+        x = -1;
+        y = k;
+        return 0;
     }
-    return -1;
+    else if(k < 2 * n) {
+        // cout << "i / 4 = 1 : " << i % 4 << ' ';
+        x = k % 4;
+        y = n;
+        return 1;
+    }
+    else if(k < 3 * n) {
+        // cout << "i / 4 = 2 : " << i % 4 << ' ';
+        x = n;
+        y = k % 4;
+        return 2;
+    }
+    else {
+        // cout << "i / 4 = 3 : " << i % 4 << ' ';
+        x = k % 4;
+        y = -1;
+        return 3;
+    }
+
+    // cout << '\n';
 }
 int main() {
     // 여기에 코드를 작성해주세요.
@@ -61,31 +59,28 @@ int main() {
     cin >> k;
     k--;
 
-    // print();
-
     dir = findDir(k);
     vector<pair<int, int>> cur;
     while(1) {
         x = x + dx[dir];
         y = y + dy[dir];
 
-
         if(x < 0 || y < 0 || x >= n || y >= n) {
             break;
         }
-        cur.push_back({x, y});
-        answer++;
         
-        if(map[x][y] == '\\') {
+        cur.push_back({x, y});
 
+        if(map[x][y] == '\\') {
+            answer++;
             if(dir == 0) dir = 3;
             else if(dir == 1) dir = 2;
             else if(dir ==2) dir = 1;
             else 
                 dir = 0;
         }
-
         else if(map[x][y] == '/') {
+            answer++;
             if(dir == 0) dir = 1;
             else if(dir == 1) dir = 0;
             else if(dir ==2) dir = 3;
