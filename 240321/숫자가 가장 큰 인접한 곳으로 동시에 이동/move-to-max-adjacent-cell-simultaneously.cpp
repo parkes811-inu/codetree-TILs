bf6aa0afe5aff80c09a4f7b615848b4e1f513ee7
박은서 <75926505+parkes811-inu@users.noreply.github.com>
2024-03-21 12:57:25 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240321/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.cpp
+++ 240321/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.cpp
@@ -1,66 +1,87 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
-
 int n, m, t;
-vector<pair<int, int>> marbles;
-int grid[22][22];
+vector<pair<int, int>> marble;
+
+int map[22][22];
+int count[22][22];
+int nextCount[22][22];
+int answer;
 // 상하좌우 순서
 int dx[4] = {-1, 1, 0, 0};
 int dy[4] = {0, 0, -1, 1};
 
-bool in_range(int x, int y) {
-    return x >= 0 && x < n && y >= 0 && y < n;
+void init() {
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            nextCount[i][j] = 0;
+        }
+    }
+}
+void print() {
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            //cout << nextCount[i][j] << ' ';
+            if(nextCount[i][j] == 1) {
+                answer++;
+            }
+        }
+        //cout << '\n';
+    }
+}
+bool InRange(int x, int y) {
+    return (x >= 0 && y >= 0 && x < n && y < n);
 }
 
-void simulate() {
-    vector<vector<int>> count(n, vector<int>(n, 0));
-    for (auto &marble : marbles) {
-        int x = marble.first, y = marble.second;
+void moveMarble() {
+
+    for(int i = 0; i < marble.size(); i++) {
+        int x = marble[i].first, y = marble[i].second;
         int max_val = -1;
         pair<int, int> next_pos = {x, y};
 
         for (int i = 0; i < 4; ++i) {
             int nx = x + dx[i], ny = y + dy[i];
-            if (in_range(nx, ny) && grid[nx][ny] > max_val) {
+            if (InRange(nx, ny) && map[nx][ny] > max_val) {
                 next_pos = {nx, ny};
-                max_val = grid[nx][ny];
+                max_val = map[nx][ny];
             }
         }
         
         count[next_pos.first][next_pos.second]++;
     }
-
-    marbles.clear();
+    marble.clear();
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
             if (count[i][j] == 1) {
-                marbles.push_back({i, j});
+                marble.push_back({i, j});
             }
         }
     }
 }
 
 int main() {
+    // 여기에 코드를 작성해주세요.
     cin >> n >> m >> t;
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            cin >> grid[i][j];
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cin >> map[i][j];
         }
     }
-    
-    for (int i = 0; i < m; ++i) {
+    for(int i = 0; i < m; i++) {
         int x, y;
         cin >> x >> y;
-        marbles.push_back({x - 1, y - 1});
-    }
-    
-    while (t--) {
-        simulate();
+        marble.push_back({x - 1, y - 1});        
     }
 
-    cout << marbles.size() << endl;
+    while(t--) {
+        moveMarble();
+    }
+    print();
+    //cout << "======================\n";
+    cout << marble.size();
     return 0;
 }
