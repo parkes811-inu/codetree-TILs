38af18719cdadc715d6231d5856ee01fd8c3d217
박은서 <75926505+parkes811-inu@users.noreply.github.com>
2024-03-26 01:36:53 +0900

    Title: 안전 지대, Time: 14ms, Memory: 0MB, Status: Passed - CodeTree

--- 240326/안전 지대/comfort-zone.cpp
+++ 240326/안전 지대/comfort-zone.cpp
@@ -17,20 +17,19 @@ bool InRange(int x, int y) {
     return (x >= 0 && y >= 0 && x < n && y < m); // m으로 수정
 }
 
-int dfs(int x, int y) {
+void dfs(int x, int y) {
     visited[x][y] = true;
-    int area = 1; // 현재 칸을 포함해야 하므로 1부터 시작
 
     for(int i = 0; i < 4; i++) {
         int nx = x + dx[i];
         int ny = y + dy[i];
 
         if(InRange(nx, ny) && map[nx][ny] > 0 && !visited[nx][ny]) {
-            area += dfs(nx, ny);
+            dfs(nx, ny);
         }
     }
 
-    return area;
+    return;
 }
 
 void init(int k) {
