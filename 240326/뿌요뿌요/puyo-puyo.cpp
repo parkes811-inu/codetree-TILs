e7d9094abe8475724bf29c49570594b7140efddd
박은서 <75926505+parkes811-inu@users.noreply.github.com>
2024-03-26 02:47:21 +0900

    Title: 뿌요뿌요, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240326/뿌요뿌요/puyo-puyo.cpp
+++ 240326/뿌요뿌요/puyo-puyo.cpp
@@ -2,65 +2,82 @@
 #include <vector>
 #include <algorithm>
 
+#define MAX_NUM 100
+#define DIR_NUM 4
+
 using namespace std;
 
 int n;
-int x, y;
-int blockCounter = 0;
-int counter = 0;
+int grid[MAX_NUM][MAX_NUM];
+bool visited[MAX_NUM][MAX_NUM];
 
-int dx[4] = {1, 0, -1, 0};
-int dy[4] = {0, 1, 0, -1};
+int max_block;
+int bomb_cnt;
 
-vector <int> vec;
+int curr_block_num;
 
-int arr[101][101];
-bool visited[101][101];
-
-bool inRange(int x, int y) {
-    return (x >= 0 && x < n && y >= 0 && y < n);
+// 탐색하는 위치가 격자 범위 내에 있는지 여부를 반환합니다.
+bool InRange(int x, int y) {
+    return x >= 0 && x < n && y >= 0 && y < n;
 }
 
-void dfs(int x, int y) {
-    int curr = arr[x][y];
-    for (int i = 0; i < 4; i++) {
-        int nx = x + dx[i];
-        int ny = y + dy[i];
-        if (inRange(nx, ny) && arr[nx][ny] == curr && !visited[nx][ny]) {
-            counter++;
-            visited[nx][ny] = 1;
-            dfs(nx, ny);
+// 탐색하는 위치로 움직일 수 있는지 여부를 반환합니다.
+bool CanGo(int x, int y, int color) {
+    if(!InRange(x, y))
+        return false;
+
+    if(visited[x][y] || grid[x][y] != color)
+        return false;
+
+    return true;
+}
+
+void DFS(int x, int y) {
+    //0: 오른쪽, 1: 아래쪽, 2: 왼쪽, 3: 위쪽
+    int dx[DIR_NUM] = {0, 1, 0, -1};
+    int dy[DIR_NUM] = {1, 0, -1, 0};
+
+    // 네 방향에 각각에 대하여 DFS 탐색을 합니다.
+    for(int dir = 0; dir < DIR_NUM; dir++) {
+        int new_x = x + dx[dir];
+        int new_y = y + dy[dir];
+
+        if(CanGo(new_x, new_y, grid[x][y])){
+            visited[new_x][new_y] = true;
+            // 블럭이 하나 추가됩니다.
+            curr_block_num++;
+            DFS(new_x, new_y);
         }
     }
 }
 
 int main() {
     cin >> n;
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            cin >> grid[i][j];
 
-    if (n == 1) {
-        cout << 0 << " " << 1;
-        return 0;
-    }
+    // 격자의 각 위치에서 탐색을 시작할 수 있는 경우
+    // 한 블럭에 대한 DFS 탐색을 수행합니다.
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(!visited[i][j]) {
+                // 해당 블럭을 방문할 수 있는 경우 visited 배열을 갱신하고
+                // 새로운 블럭을 탐색한다는 의미로 curr_block_num을 1으로 갱신합니다.
+                visited[i][j] = true;
+                curr_block_num = 1;
+                
+                DFS(i, j);
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cin >> arr[i][j];
-        }
-    }
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (!visited[i][j]) {
-                counter = 0;
-                dfs(i,j);
-                if (counter >= 2)
-                    vec.push_back(counter);
-                if (counter >= 4)
-                    blockCounter++;
+                // 한 블럭 묶음에 대한 탐색이 끝난 경우 답을 갱신합니다.
+                if(curr_block_num >= 4)
+                    bomb_cnt++;
+                
+                max_block = max(max_block, curr_block_num);
             }
         }
     }
-    sort(vec.rbegin(), vec.rend());
-    cout << blockCounter << " " << vec[0];
+    
+    cout << bomb_cnt << " " << max_block;
     return 0;
 }
