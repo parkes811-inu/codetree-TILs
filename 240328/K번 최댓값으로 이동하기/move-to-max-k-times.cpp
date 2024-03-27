c8b09c158b7937e80dd2239de53afa431f5117fa
박은서 <75926505+parkes811-inu@users.noreply.github.com>
2024-03-28 00:16:43 +0900

    Title: K번 최댓값으로 이동하기, Time: 12ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240328/K번 최댓값으로 이동하기/move-to-max-k-times.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int n, k;
+pair<int, int> cur;
+
+queue<pair<int, int>> q;
+
+int map[102][102];
+bool visited[102][102];
+
+int answer;
+
+int start;
+int maxNum;
+
+int dx[4] = {0, 0, -1, 1};
+int dy[4] = {-1, 1, 0, 0};
+
+bool InRange(int x, int y) {
+    return (x > 0 && y > 0 && x <= n && y <= n);
+}
+
+void bfs() {
+
+    while(!q.empty()) {
+
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+
+        for(int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if(InRange(nx, ny) && !visited[nx][ny] && map[nx][ny] < start) {
+                if(map[nx][ny] > maxNum) {
+                    maxNum = map[nx][ny];
+                    cur.first = nx;
+                    cur.second = ny;
+                }
+                visited[nx][ny] = true;
+                q.push({nx, ny});
+            }
+
+            if(map[nx][ny] == maxNum) {
+                if(cur.first > nx) {
+                    cur.first = nx;
+                    cur.second = ny;
+                }
+                else if(cur.first == nx) {
+                    if(cur.second > ny) {
+                        cur.first = nx;
+                        cur.second = ny;
+                    }
+                }
+            }
+        }
+    }
+    return;
+}
+
+void init() {
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            visited[i][j] = false;
+        }
+    }
+    while(!q.empty()) {
+        q.pop();
+    }
+}
+
+int main() 
+{
+    // 여기에 코드를 작성해주세요.
+
+    cin >> n >> k;
+
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            cin >> map[i][j];
+        }
+    }
+
+    int x, y;
+    cin >> x >> y;
+    cur.first = x, cur.second = y;
+
+    for(int t = 0; t < k; t++) {
+        maxNum = -1;
+        visited[cur.first][cur.second] = true;
+        q.push({cur.first, cur.second});
+        start = map[cur.first][cur.second];
+        bfs();
+        //if(maxNum == -1) {
+            //cout << cur.first << ' ' << cur.second << '\n';
+            //return 0;
+        //}
+        init();
+    }
+    cout << cur.first << ' ' << cur.second << '\n';
+
+    return 0;
+}
