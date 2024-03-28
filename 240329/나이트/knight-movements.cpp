3165df0bb834ac8df2bf66ba82f7d8f9ba16e38b
박은서 <75926505+parkes811-inu@users.noreply.github.com>
2024-03-29 02:34:27 +0900

    Title: 나이트, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240329/나이트/knight-movements.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int dist[101][101];
+bool visited[101][101] = {false, };
+queue<pair<int, int>> q;
+
+int dx[8] = {-1, -2, -2, -1, 1, 2, 2, 1};
+int dy[8] = {-2, -1, 1, 2, 2, 1, -1, -2};
+
+int n;
+int r1, c1, r2, c2;
+
+bool InRange(int x, int y) {
+    return (x > 0 && y > 0 && x <= n && y <= n);
+}
+
+void bfs() {
+
+    while(!q.empty()) {
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+
+        for(int i = 0; i < 8; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if(InRange(nx, ny) && !visited[nx][ny]) {
+                visited[nx][ny] = true;
+                dist[nx][ny] = dist[x][y] + 1;
+                q.push({nx, ny});
+            }
+        }
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+    cin >> r1 >> c1 >> r2 >> c2;
+
+    q.push({r1, c1});
+    visited[r1][c1] = true;
+    bfs();
+
+    cout << dist[r2][c2];
+    return 0;
+}
